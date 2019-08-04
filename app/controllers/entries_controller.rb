f0a952f5651a8ccf7771cfb4514ab6b3bf86c0f5
yuhei-ida <yuuhei0311>
2019-08-04 21:59:31 +0900

    エントリー登録機能を作成

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,31 @@
 class EntriesController < ApplicationController
 
     def index
-        
+        @entries = Entry.all
     end
+
+
+    def new
+        @entry = Entry.new
+    end
+
+    def create
+        @entry = Entry.new(create_params)
+        @entry.createby_id = current_account.id
+
+        if @entry.save
+            flash[:info] = "新しいエントリーを登録しました"
+            redirect_to entries_path
+        else
+            render "new"
+        end
+    end
+
+    private
+        def create_params
+            params.require(:entry).permit(:title,:contents)
+        end
+
+    
 
 end
